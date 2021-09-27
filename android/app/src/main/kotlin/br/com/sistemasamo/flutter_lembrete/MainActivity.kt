42a94549fa917793a5459dba599a3dcb389e1038
Bruno Alves de Vasconcelos Silva <contato@alvesbruno.com>
2021-09-27 18:22:09 -0300

    feat(app): add basic files app

---
+++ android/app/src/main/kotlin/br/com/sistemasamo/flutter_lembrete/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.sistemasamo.flutter_lembrete
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RememberApp());
+}
+
+class Item {
+  Item({
+    required this.expandedValue,
+    required this.headerValue,
+    this.isExpanded = false,
+  });
+
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+}
+
+class RememberApp extends StatelessWidget {
+  const RememberApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Lembretes de contas a pagar'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Item> _data = [
+    Item(
+      headerValue: '08/07 Conta de luz',
+      expandedValue: 'This is item number 1',
+    ),
+    Item(
+      headerValue: '10/07 Conta de gás',
+      expandedValue: 'This is item number 1',
+    ),
+    Item(
+      headerValue: '11/07 Conta de internet',
+      expandedValue: 'This is item number 1',
+    ),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[_billPayments()],
+        ),
+      )
+    );
+  }
+
+  Widget _billPayments() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              title: Text(item.headerValue),
+            );
+          },
+          body: ListTile(
+              title: Text(item.expandedValue),
+              subtitle:
+              const Text('To delete this panel, tap the trash can icon'),
+              trailing: const Icon(Icons.delete),
+              onTap: () {
+                setState(() {
+                  _data.removeWhere((Item currentItem) => item == currentItem);
+                });
+              }),
+          isExpanded: item.isExpanded,
+        );
+      }).toList(),
+    );
+  }
+}
