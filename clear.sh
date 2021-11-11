23e65a0d5524e8903decf5b8ec2cf4960c7f2919
contato@alvesbruno.com <Bruno Alves>
2021-11-11 19:20:17 -0300

    feat(deploy): build app flutter in ci workflow
    wip(ci): ckeckout google json
    
    wip(ci): ckeckout google json
    
    wip(ci): ckeckout google json
    
    wip(ci): ckeckout google json
    
    wip(ci): ckeckout google json
    
    wip(ci): ckeckout google json
    
    wip(ci): ckeckout google json
    
    wip(ci): ckeckout google json

--- clear.sh
+++ clear.sh
@@ -2,11 +2,17 @@
 rm -rf ~/Library/Developer/Xcode/DerivedData/
 rm -rf ~/Library/Caches/CocoaPods/
 rm -rf clone/ios/Pods/
+rm -rf .dart_tool*
+rm -rf build*
+rm .flutter-plugins*
+rm .packages*
+
 pod cache clean --all
 
 # clear flutter - skip the next 3 lines if you're not using flutter
 flutter clean
 flutter pub get
+
 cd ios
 
 # run pod install
